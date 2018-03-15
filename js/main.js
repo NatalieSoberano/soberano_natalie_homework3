c6d7b1eb85ff62f5d75f8ea11906df3f030beab7
Natalie Soberano <nataliesoberano@natalies-mbp-2.fc.ca>
2018-03-15 11:15:55 -0400

    most recent changes reviewed by Trevor in class

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@
   //this is a fat arrow function
 console.log('Starship Trooper Game');
 })();
+
+window.onload = function() {
+    var canvas = document.querySelector("canvas");
+    var ctx = canvas.getContext("2d");
+    var img = document.querySelector("#warrior_bug");
+   ctx.drawImage(img, 10, 10);
+};
+
+window.onload = function() {
+    var canvas = document.querySelector("canvas");
+    var ctx = canvas.getContext("2d");
+    var img = document.querySelector(".gun");
+   ctx.drawImage(img, 10, 10);
+};
+
