28c590b5aa3893892bd7b9876c04b06356c780b7
Natalie Soberano <nataliesoberano@Natalies-MacBook-Pro-2.local>
2018-03-28 16:15:22 -0400

    these are the changes I made to the elements on the canvas - specifically getting the bug, gun and laser to appear and the bug and laswer to move

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 (() => {
   //this is a fat arrow function
 console.log('Starship Trooper Game');
-})();
-
-window.onload = function() {
-    var canvas = document.querySelector("canvas");
+	var canvas = document.querySelector("canvas");
     var ctx = canvas.getContext("2d");
     var img = document.querySelector("#warrior_bug");
-   ctx.drawImage(img, 10, 10);
+    var gun = document.querySelector(".gun");
+    var img2 = document.querySelectorAll(".gun");
+    var startPoint = 10;
+    var laser = document.querySelector("#laser");
+    var laserStart = 703;
+
+function Draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+   	ctx.drawImage(img, startPoint, 200, 130, 70);
+   	//img2.forEach((gun) => {ctx.drawImage(gun 470, 200, 130, 70)});
+	ctx.drawImage (gun, 850, 200, 130, 70);
+	 startPoint+=4;
+	   if (startPoint > canvas.width) {startPoint = 0}
+	   window.requestAnimationFrame(Draw);
+	ctx.drawImage (laser, laserStart, 225, 150, 10);
+		laserStart+=5;
+		if (laserStart > canvas.width) {laserStart = 0}
+		window.requestAnimationFrame(Draw);
 };
 
-window.onload = function() {
-    var canvas = document.querySelector("canvas");
-    var ctx = canvas.getContext("2d");
-    var img = document.querySelector(".gun");
-   ctx.drawImage(img, 10, 10);
-};
+window.requestAnimationFrame(Draw);
+
+})();
+
 
